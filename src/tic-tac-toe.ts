56a8ce520487a9f12043ec25ced2574821b36d69
hartphoenix <161499832+hartphoenix@users.noreply.github.com>
2026-02-03 13:53:44 -0500

    added all win conditions to getWinner

--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -23,19 +23,32 @@ export function createGame(): GameState {
   };
 }
 
-export const changePlayer = (player: Player):Player => {
+export const changePlayer = (player: Player): Player => {
   if (player === "X") return "O"
   if (player === "O") return "X"
 }
 
 export function makeMove(state: GameState, position: number): GameState {
-  const newBoard:Cell[] = [...state.board]
+  const newBoard: Board = [...state.board]
   newBoard[position] = state.currentPlayer
+  const winner = getWinner({ board: newBoard, currentPlayer: state.currentPlayer })
+  if (winner) {
+    console.log("WINNER:", winner)
+  }
   const newPlayer = changePlayer(state.currentPlayer)
-  const newState:GameState = { board: newBoard, currentPlayer: newPlayer} 
+  const newState: GameState = { board: newBoard, currentPlayer: newPlayer }
   return newState
 }
 
 export function getWinner(state: GameState): Player | null {
+  if ((state.board[0] && state.board[0] === state.board[1] && state.board[1] === state.board[2]) // horizontal
+    || (state.board[3] && state.board[3] === state.board[4] && state.board[4] === state.board[5]) // horizontal
+    || (state.board[6] && state.board[6] === state.board[7] && state.board[7] === state.board[8]) // horizontal
+    || (state.board[0] && state.board[0] === state.board[4] && state.board[4] === state.board[8]) // diagonal
+    || (state.board[2] && state.board[2] === state.board[4] && state.board[4] === state.board[6]) // diagonal
+    || (state.board[0] && state.board[0] === state.board[3] && state.board[3] === state.board[6]) // vertical
+    || (state.board[1] && state.board[1] === state.board[4] && state.board[4] === state.board[7]) // vertical
+    || (state.board[2] && state.board[2] === state.board[5] && state.board[5] === state.board[8]) // vertical
+  ) { return state.currentPlayer }
   return null;
 }
