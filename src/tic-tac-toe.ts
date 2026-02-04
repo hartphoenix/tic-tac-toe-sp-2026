e6d33d4ed4e6d7c5171ab33c724ec5d515fdba89
hartphoenix <161499832+hartphoenix@users.noreply.github.com>
2026-02-04 14:49:14 -0500

    created 3D version of game, extended game logic to check 27 cells and 49 win conditions, implemented 3D rendering in React 3D Fiber

--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -1,24 +1,29 @@
+import { winLines } from './winLogic'
 export type Player = "X" | "O";
 
 export type Cell = Player | null;
 
-// Board is a 3x3 grid, represented as a 9-element array.
-// Indices map to positions:
-//  0 | 1 | 2
-//  ---------
-//  3 | 4 | 5
-//  ---------
-//  6 | 7 | 8
-export type Board = [Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell];
+export type Board = [
+  Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell,
+  Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell,
+  Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell, Cell,
+];
+
+type WinState = Player | 'tie' | undefined
 
 export type GameState = {
   board: Board;
   currentPlayer: Player;
+  endState?: WinState
 };
 
 export function createGame(): GameState {
   return {
-    board: [null, null, null, null, null, null, null, null, null],
+    board: [
+      null, null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null, null
+    ],
     currentPlayer: "X",
   };
 }
@@ -29,8 +34,8 @@ export const changePlayer = (player: Player): Player => {
 }
 
 export function makeMove(state: GameState, position: number): GameState {
-  if (position < 0 || position > 8) {
-    throw new Error("Position must be between 0 and 8")
+  if (position < 0 || position > 26) {
+    throw new Error("Position must be between 0 and 26")
   }
   if (position % 1 !== 0) {
     throw new Error("Position must be an integer")
@@ -49,7 +54,7 @@ export function makeMove(state: GameState, position: number): GameState {
     console.log("WINNER:", winner)
   }
   const newPlayer = changePlayer(state.currentPlayer)
-  const newState: GameState = { board: newBoard, currentPlayer: newPlayer }
+  const newState: GameState = { board: newBoard, currentPlayer: newPlayer, endState: winner ?? undefined }
   return newState
 }
 
@@ -60,11 +65,6 @@ const tripleWins = (state: GameState, n: number[]): boolean => {
 }
 
 export function getWinner(state: GameState): Player | null {
-  const winLines: Array<Array<number>> = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontals
-    [0, 3, 6], [1, 4, 7], [2, 5, 8], // verticals
-    [0, 4, 8], [2, 4, 6] //          diagonals
-  ]
   for (const line of winLines) {
     if (tripleWins(state, line)) return state.board[line[0]]
   }
