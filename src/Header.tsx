a5591efdb5681ad9c13973792320e86455d2e3ca
hartphoenix <161499832+hartphoenix@users.noreply.github.com>
2026-02-05 18:57:35 -0500

    lobby view manages multiple games and creates new games that persist in server between browser refreshes

--- src/Header.tsx
+++ src/Header.tsx
@@ -7,22 +7,24 @@ export const Header = (
         reset: () => void
     }
 ): JSX.Element => {
-    if ((gameState.endState !== null) && gameState.endState !== "tie") return (
-        <div className="header">
-            <p>{gameState.endState === "X" ? "RED" : "BLUE"} wins!</p>
-            <button className="reset-btn" onClick={reset}>
-                Play again?
-            </button>
-        </div>
-    )
-    if (!gameState.board.some(pos => pos === null)) return (
-        <div className="header">
-            <p>Tie game!</p>
-            <button className="reset-btn" onClick={reset}>
-                Play again?
-            </button>
-        </div>
-    )
+    if ((gameState.endState !== null) && gameState.endState !== "tie")
+        return (
+            <div className="header">
+                <p>{gameState.endState === "X" ? "RED" : "BLUE"} wins!</p>
+                <button className="reset-btn" onClick={reset}>
+                    Play again?
+                </button>
+            </div>
+        )
+    if (!gameState.board.some(pos => pos === null))
+        return (
+            <div className="header">
+                <p>Tie game!</p>
+                <button className="reset-btn" onClick={reset}>
+                    Play again?
+                </button>
+            </div>
+        )
     else return (
         <div className="header">
             <p>{gameState.currentPlayer === "X" ? "RED" : "BLUE"}'s move</p>
