a09585ffe07b16be274bbc40d8e4e24b2f8e2d42
hartphoenix <161499832+hartphoenix@users.noreply.github.com>
2026-02-06 19:20:02 -0500

    refactored /move routes to use websockets, broadcast to all users, and render instantly in all clients

--- gameStore.ts
+++ gameStore.ts
@@ -4,54 +4,54 @@ import { getWinner } from './src/tic-tac-toe'
 export const games: Map<string, GameState> = new Map()
 
 export function createGame(): GameState {
-    const newGame: GameState = {
-        board: [
-            null, null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null, null,
-            null, null, null, null, null, null, null, null, null
-        ],
-        currentPlayer: "X",
-        endState: null,
-        id: crypto.randomUUID()
-    }
-    games.set(newGame.id, newGame)
-    return newGame
+  const newGame: GameState = {
+    board: [
+      null, null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null, null,
+      null, null, null, null, null, null, null, null, null
+    ],
+    currentPlayer: "X",
+    endState: null,
+    id: crypto.randomUUID()
+  }
+  games.set(newGame.id, newGame)
+  return newGame
 }
 
 export const changePlayer = (player: Player): Player => {
-    if (player === "X") return "O"
-    else return "X"
+  if (player === "X") return "O"
+  else return "X"
 }
 
 export function makeMove(state: GameState, position: number): GameState {
-    if (position < 0 || position > 26) {
-        throw new Error("Position must be between 0 and 26")
-    }
-    if (position % 1 !== 0) {
-        throw new Error("Position must be an integer")
-    }
-    if (state.board[position] !== null) {
-        throw new Error("Position is already occupied")
-    }
-    if (state.endState !== null) {
-        throw new Error("Game is already over")
-    }
+  if (position < 0 || position > 26) {
+    throw new Error("Position must be between 0 and 26")
+  }
+  if (position % 1 !== 0) {
+    throw new Error("Position must be an integer")
+  }
+  if (state.board[position] !== null) {
+    throw new Error("Position is already occupied")
+  }
+  if (state.endState !== null) {
+    throw new Error("Game is already over")
+  }
 
-    const newBoard: Board = [...state.board]
-    newBoard[position] = state.currentPlayer
-    const newPlayer = changePlayer(state.currentPlayer)
-    const winner = getWinner({
-        board: newBoard,
-        currentPlayer: state.currentPlayer,
-        endState: state.endState,
-        id: state.id
-    })
-    const newState: GameState = {
-        board: newBoard,
-        currentPlayer: newPlayer,
-        endState: winner,
-        id: state.id
-    }
-    games.set(state.id, newState)
-    return newState
+  const newBoard: Board = [...state.board]
+  newBoard[position] = state.currentPlayer
+  const newPlayer = changePlayer(state.currentPlayer)
+  const winner = getWinner({
+    board: newBoard,
+    currentPlayer: state.currentPlayer,
+    endState: state.endState,
+    id: state.id
+  })
+  const newState: GameState = {
+    board: newBoard,
+    currentPlayer: newPlayer,
+    endState: winner,
+    id: state.id
+  }
+  games.set(state.id, newState)
+  return newState
 }
